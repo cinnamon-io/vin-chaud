ec0df4ca7b1826b02c5e16be9bd1048c2029e80d
yongju shin <ygp007@naver.com>
2021-12-28 17:11:20 +0900

    ADD: examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { Hello } from '@src/components/Helloo';
+import logo from '@public/images/logo512.png';
+import './styles/index.scss';
 
 function App() {
   return (
     <div>
       <Hello />
+      <img src={logo} alt="" />
+      <div
+        style={{
+          height: 50,
+          background: `url(${logo}) 50% 50% no-repeat`,
+          backgroundSize: 'auto 100%',
+        }}
+      />
+      <div className="logo">
+        <p>
+          React <span>settup</span>
+        </p>
+      </div>
     </div>
   );
 }
