f450840ccfbc286ae29c2e603ac65d1ed1c0febd
yongju shin <ygp007@naver.com>
2021-12-28 16:33:25 +0900

    ADD: Public path

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,17 +1,21 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, '../dist'),
     filename: 'bundle.[hash].js',
-    publicPath: '/',
+    publicPath: ASSET_PATH,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
+      '@public': path.resolve(__dirname, '../public'),
       '@src': path.resolve(__dirname, '../src/'),
     },
   },
@@ -42,6 +46,10 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    // assets public path 설정
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
     }),
   ],
 };
