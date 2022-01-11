51d230af45842ef86ad57c0f87855fec31476b2b
yongju shin <ygp007@naver.com>
2022-01-11 13:34:23 +0900

    UPDATE: example

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,112 @@
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import { Hello } from '@src/components/Helloo';
 import logo from '@public/images/logo512.png';
 import lockUrl, { ReactComponent as Lock } from '@public/images/icon-interlock-success.svg';
 import './styles/index.scss';
+import React from 'react';
 
+// Layout 위주 route 컴포넌트 구성.
+const DefaultLayout = () => {
+  return (
+    <>
+      <header>
+        <h1>Vin-CHAUD</h1>
+        Header
+      </header>
+      <main>
+        <article>
+          <Outlet />
+        </article>
+      </main>
+      <footer>Footer</footer>
+    </>
+  );
+};
+const SideLayout = () => {
+  return (
+    <>
+      <header>
+        <h1>Vin-CHAUD</h1>
+        Header
+      </header>
+      <main>
+        <aside>Side</aside>
+        <article>
+          <Outlet />
+        </article>
+      </main>
+      <footer>Footer</footer>
+    </>
+  );
+};
+const RouteWithLayout = ({
+  path,
+  layout,
+  children,
+  ...rest
+}: {
+  path: string;
+  layout: React.ReactElement;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Route path={path} element={layout} {...rest}>
+      {children}
+    </Route>
+  );
+};
 function App() {
   return (
-    <div>
-      <h1>Vin-CHAUD</h1>
-
-      <Hello />
-      <h2>url</h2>
-      <img src={logo} alt="" />
-      <h2>style bg</h2>
-      <div
-        style={{
-          height: 50,
-          background: `url(${logo}) 50% 50% no-repeat`,
-          backgroundSize: 'auto 100%',
-        }}
-      />
-      <h2>css bg</h2>
-      <div className="logo">
-        <p>
-          React <span>settup</span>
-        </p>
-      </div>
-      <h2>svg url</h2>
-      <img src={lockUrl} alt="" />
-      <h2>svg component</h2>
-      <Lock />
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">홈</Link>
+          </li>
+          <li>
+            <Link to="/detail">상세</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <RouteWithLayout path="/" layout={<DefaultLayout />}>
+          <Route
+            index
+            element={
+              <>
+                <Hello />
+                <p>Home</p>
+                <h2>image 사용 예시</h2>
+                <h3>url</h3>
+                <img src={logo} alt="" />
+                <h3>style bg</h3>
+                <div
+                  style={{
+                    height: 50,
+                    background: `url(${logo}) 50% 50% no-repeat`,
+                    backgroundSize: 'auto 100%',
+                  }}
+                />
+                <h3>css bg</h3>
+                <div className="logo">
+                  <p>
+                    React <span>settup</span>
+                  </p>
+                </div>
+                <h3>svg url</h3>
+                <img src={lockUrl} alt="" />
+                <h3>svg component</h3>
+                <Lock />
+              </>
+            }
+          />
+        </RouteWithLayout>
+        <RouteWithLayout path="detail" layout={<SideLayout />}>
+          <Route index element={<>상세</>} />
+        </RouteWithLayout>
+        <Route path="*" element={<>404</>} />
+      </Routes>
+    </>
   );
 }
 
